849e9efd22d1d80902f17c8f94097f13ecc78fc1
Ryan Smith <Rwsmith8692@gmail.com>
2019-09-06 19:26:52 -0500

    Update ChallengesSet02.cs

--- weeklyChallenges02/ChallengesSet02.cs
+++ weeklyChallenges02/ChallengesSet02.cs
@@ -13,39 +13,71 @@ namespace weeklyChallenges02
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.GetLength(0) % 2 == 0)
+            { return true; }
+
+            else
+            { return false; }
         }
 
         public bool IsNumberEven(int number)
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            { return true; }
+
+            else
+            { return false; }
         }
 
 
         public bool IsNumberOdd(int num)
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            { return true; }
+
+            else
+            { return false; }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            { return numbers.Max() + numbers.Min(); }
         }
 
         public int GetLengthOfShortestString(string str1, string str2)
         {
-            throw new NotImplementedException();
+            int num1 = str1.Length;
+            int num2 = str2.Length;
+            if (num1 <= num2)
+            { return num1; }
+            else
+            { return num2; }
 
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            { return 0; }
+            else
+            { return numbers.Sum(); }
         }
 
         public int SumEvens(int[] numbers)
-        {
-            throw new NotImplementedException();
+        {   if (numbers == null)
+            { return 0; }
+            int total = 0;
+            foreach (var element in numbers)
+            {
+                if (element % 2 == 0)
+                { total += element; }
+               
+            }
+            return total;
         }
 
         public bool IsSumOdd(List<int> numbers)
